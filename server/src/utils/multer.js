124d6651d817771f65a01427128d1b36daa06bd3
Abhishek yadav <studyandrotech@gmail.com>
2026-06-21 14:44:55 +0530

    fix: change multer dest to relative path

--- server/src/utils/multer.js
+++ server/src/utils/multer.js
@@ -1,5 +1,7 @@
-const multer = require("multer")
+const multer = require("multer");
+const path = require("path");
 
-const upload = multer({dest:"/upload"})
+// Isse 'upload' folder aapke project ke root folder ke andar banega
+const upload = multer({ dest: path.join(__dirname, "../../upload") });
 
-module.exports = upload
+module.exports = upload;
